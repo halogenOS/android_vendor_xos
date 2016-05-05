2a80fd8dc1ed67f0616dc9f584488d335a7ec9a6
Neil Agarwal <regalstreak@gmail.com>
2016-05-05 19:05:30 +0000

    config: ro.xos.verion --> ro.XOS.version

--- config/common.mk
+++ config/common.mk
@@ -108,7 +108,7 @@ XOS_MOD_VERSION := XOS_$(XOS_BUILD)_$(ANDROID_VERSION)_$(shell date +%Y%m%d)_$(X
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
-    ro.xos.version=$(XOS_VERSION) \
+    ro.XOS.version=$(XOS_VERSION) \
     ro.mod.version=$(XOS_BUILD_TYPE)-v0.1-alpha \
     
 $(call inherit-product-if-exists, vendor/extra/product.mk)
