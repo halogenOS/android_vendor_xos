5ce2dfaef43f546509bc81bacd487c14d151ceaa
Simao Gomes Viana <xdevs23@outlook.com>
2017-03-12 14:57:09 +0100

    Get back safemode command
    Partially revert "common: Don't ship useless init.d scripts"
    
    This reverts commit b7131d4d23a554d4b1e68a8170685295a3db9c59.
    
    Change-Id: I717a2022d929bd3c9aff0c62262fa45a9c9d013d

--- config/common.mk
+++ config/common.mk
@@ -38,6 +38,9 @@ PRODUCT_COPY_FILES += \
     vendor/xos/prebuilt/common/bin/blacklist:system/addon.d/blacklist \
     vendor/xos/prebuilt/common/bin/whitelist:system/addon.d/whitelist \
 
+# Safemode script
+PRODUCT_COPY_FILES += \
+	vendor/xos/prebuilt/common/bin/safemode:system/bin/safemode
 
 # Bootanimation
 PRODUCT_COPY_FILES += \
@@ -48,6 +51,10 @@ PRODUCT_COPY_FILES += \
     vendor/xos/prebuilt/common/bin/sysinit:system/bin/sysinit \
     vendor/xos/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/xos/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
+
+# xosinit.d
+PRODUCT_COPY_FILES += \
+    vendor/xos/prebuilt/common/etc/xosinit.d/01safemode:system/etc/xosinit.d/01safemode
 
 # Init file
 PRODUCT_COPY_FILES += \
