92d4e0d5cae68246d615092f41b970a5726e6341
Simao Gomes Viana <xdevs23@outlook.com>
2017-03-12 14:49:10 +0100

    essentials: cleanup
    Change-Id: I4fb4b4417f47770b47ad1fa6e5f3c1d8c355d4b5

--- config/essentials.mk
+++ config/essentials.mk
@@ -5,11 +5,8 @@ PRODUCT_PACKAGES += \
 	libhidcommand_jni \
 	libavformat \
 	libcryptfs_hw \
-	libffmpeg_extractor \
-	libffmpeg_omx \
 	libffmpeg_utils \
 	libjni_eglfence \
 	libjni_jpegstream \
 	libprotobuf-cpp-full \
-	librsjni \
-	media_codecs_ffmpeg.xml
+	librsjni
