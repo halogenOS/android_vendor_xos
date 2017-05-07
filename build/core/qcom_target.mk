14ee7877c6bd7f15be68ed7c2882d7af7e59527d
Simao Gomes Viana <xdevs23@outlook.com>
2017-05-07 11:05:11 +0200

    qcom_target: allow using custom audio hal path
    Change-Id: I82a9a1c17b51a12d77a5e073aa75b46dae348774
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -78,7 +78,11 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     endif
     endif
 
+ifeq ($(BOARD_AUDIO_HAL_PATH),)
 $(call set-device-specific-path,AUDIO,audio,hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT))
+else
+$(call set-device-specific-path,AUDIO,$(BOARD_AUDIO_HAL_PATH))
+endif
 $(call set-device-specific-path,DISPLAY,display,hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT))
 $(call set-device-specific-path,MEDIA,media,hardware/qcom/media-caf/$(QCOM_HARDWARE_VARIANT))
 
@@ -94,7 +98,11 @@ $(call bt-vendor-set-path-variant,bt-caf)
 
 else
 
+ifeq ($(BOARD_AUDIO_HAL_PATH),)
 $(call project-set-path,qcom-audio,hardware/qcom/audio/default)
+else
+$(call project-set-path,qcom-audio,$(BOARD_AUDIO_HAL_PATH))
+endif
 $(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
 $(call project-set-path,qcom-media,hardware/qcom/media/$(TARGET_BOARD_PLATFORM))
 
