0872cfd98ee09993e192b577fc13e008f5fc0c25
Simao Gomes Viana <xdevs23@outlook.com>
2017-07-14 12:39:34 +0200

    Now we are also merging LA.UM.5.8 8x98
    Change-Id: I67a949f04caf2574c56301f218df1df78e5cfda7

--- config/common.mk
+++ config/common.mk
@@ -110,7 +110,7 @@ USE_DEX2OAT_DEBUG ?= false
 ANDROID_VERSION = 7.1.2
 PLATFORM_VERSION_CODENAME = REL
 
-THE_REVISIONS := caf=LA.UM.5.6.r1-05300-89xx.0,droid=android-7.1.2_r24
+THE_REVISIONS := caf=LA.UM.5.6.r1-05300-89xx.0+LA.UM.5.8.r1-02200-8x98.0,droid=android-7.1.2_r24
 
 XOS_BUILD_DATE := $(shell date +%Y%m%d)
 
