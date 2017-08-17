271ba5c15a2f8c95de9a3c0a987f4b30e461208e
Luca Stefani <luca020400@lineageos.org>
2017-08-17 20:34:44 +0200

    vendor: build: Kill schedtool
    Change-Id: Idd6cc6679b1e84f365a5a18645590c549af20af5
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -624,14 +624,7 @@ EOF
 }
 
 function mka() {
-    case `uname -s` in
-        Darwin)
-            m -j "$@"
-            ;;
-        *)
-            mk_timer schedtool -B -n 10 -e ionice -n 7 m -j "$@"
-            ;;
-    esac
+    m -j "$@"
 }
 function cmka() {
     if [ ! -z "$1" ]; then
