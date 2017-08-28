5966c25fd8a214ebccb6d7cb54faac533ffa8962
Simao Gomes Viana <xdevs23@outlook.com>
2017-08-28 14:28:16 +0200

    Move qcom and mtk flags over to soong [1/2]
    See android_build_soong
    
    Change-Id: Ie1d8bcb679b8eebe0a8b5e44a8c84bb72a1177d8

--- build/core/mtk_target.mk
+++ build/core/mtk_target.mk
@@ -2,8 +2,5 @@
 include vendor/xos/build/core/mtk_utils.mk
 
 ifeq ($(BOARD_USES_MTK_HARDWARE),true)
-    mtk_flags := -DMTK_HARDWARE
 
-    PRIVATE_TARGET_GLOBAL_CFLAGS += $(mtk_flags)
-    PRIVATE_TARGET_GLOBAL_CPPFLAGS += $(mtk_flags)
 endif
