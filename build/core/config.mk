bcbf98a4cce2f24597a21a95a62f323e595d27d9
Simao Gomes Viana <xdevs23@outlook.com>
2019-07-14 23:03:42 +0200

    build: core: config: nuke lineage stuff using config
    Change-Id: I399a948c05442d6f934d1e6ec085ccab75aa5358

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,4 +1,6 @@
 BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/aosp/build/core/system_rro.mk
+# To exclude lineage-specific things without deleting everything
+PRODUCT_IS_LINEAGE := false
 
 # Rules for QCOM targets
 include $(TOPDIR)vendor/aosp/build/core/qcom_target.mk
